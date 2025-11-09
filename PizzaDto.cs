20cd90c84ccc80e4fca4753ac60f52db196381fe
Siraj Khan .H <sirajh2002@gmail.com>
2025-11-09 15:57:45 +0530

    Delete PizzaDto.cs

--- PizzaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PIzzaDeliveryAPI.DTOs
-{
-    public class PizzaDto
-    {
-        public  int Id  { get; set; }
-        public  string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
