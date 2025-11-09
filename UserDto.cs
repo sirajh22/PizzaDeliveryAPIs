c3c2676caacc49e73848116131d1d5cd26218331
Siraj Khan .H <sirajh2002@gmail.com>
2025-11-09 15:58:47 +0530

    Delete UserDto.cs

--- UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PIzzaDeliveryAPI.DTOs
-{
-    public class UserDto
-    {
-        public  int Id { get; set; }//only returning after saving
-        public  string Name { get; set; }
-        public  string Email { get; set; }
-    }
-}
